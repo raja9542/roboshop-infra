db22b89a87e967137d19927f949388b2e8b8061c
raja9542 <rajasekhar1banda@gmail.com>
2023-11-10 19:38:26 +0530

    separate public subnet created

--- main.tf
+++ main.tf
@@ -6,23 +6,32 @@ module "vpc" {
   cidr_block = each.value.cidr_block
 }
 
-#module "subnets" {
-#  source = "github.com/raja9542/tf-module-subnets.git"
-#  env = var.env
-#  default_vpc_id = var.default_vpc_id
-#
-#  for_each = var.subnets
-#  cidr_block = each.value.cidr_block
-#  availability_zone =each.value.availability_zone
-#  name = each.value.name
-#  vpc_id = lookup(lookup(module.vpc, each.value.vpc_name, null), vpc_id, null) # after declaring output in vpc module
-#}
+module "subnets" {
+  source = "github.com/raja9542/tf-module-subnets.git"
+  env = var.env
+  default_vpc_id = var.default_vpc_id
 
-output "vpc_id" {
-  value = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  for_each = var.subnets
+  cidr_block = each.value.cidr_block
+  availability_zone =each.value.availability_zone
+  name = each.value.name
+  vpc_id = lookup(lookup(module.vpc, each.value.vpc_name, null), vpc_id, null) # after declaring output in vpc module
 }
+
+
 #output for this
+
+#output "vpc_id" {
+#value = lookup(module.vpc, "main", null)
+#}
 #vpc_id = {
 #  "vpc_id" = "vpc-0e0952dda633abb33"
 #}
+
+#output for this
+
+#output "vpc_id" {
+#  value = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+#}
+#"vpc-0e0952dda633abb33"
 
