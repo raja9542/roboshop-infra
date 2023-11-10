b28473ab5d2e03d54363bec104c6747479049fd3
raja9542 <rajasekhar1banda@gmail.com>
2023-11-10 19:03:09 +0530

    separate public subnet created

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "network" {
+module "vpc" {
   source = "github.com/raja9542/tf-module-vpc.git"
   env = var.env
   default_vpc_id = var.default_vpc_id
@@ -6,3 +6,13 @@ module "network" {
   cidr_block = each.value.cidr_block
 }
 
+module "subnets" {
+  source = "github.com/raja9542/tf-module-subnets.git"
+  env = var.env
+  default_vpc_id = var.default_vpc_id
+  vpc_id = module.vpc.vpc_id
+  for_each = var.subnets
+  cidr_block = each.value.cidr_block
+  availability_zone =each.value.availability_zone
+  name = each.value.name
+}
