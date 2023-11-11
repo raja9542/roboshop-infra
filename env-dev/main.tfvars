87d10f392262736c00d1fcfe7897978ed595fe1d
raja9542 <rajasekhar1banda@gmail.com>
2023-11-11 12:20:03 +0530

    sub subnet module in module vpc

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -4,48 +4,40 @@ default_vpc_id = "vpc-0e9be6cf7a7cbd838"
 vpc = {
   main = {
     cidr_block = "10.0.0.0/16"
-  }
-}
+    availability_zone = ["us-east-1a", "us-east-1b"]
+    subnets    = {
+      public = {
 
-subnets = {
+        name              = "public"
+        cidr_block        = ["10.0.0.0/24", "10.0.1.0/24"]
+        internet_gw       = true
 
-  public = {
+      }
 
-    name               = "public"
-    vpc_name           = "main"
-    cidr_block         = ["10.0.0.0/24", "10.0.1.0/24"]
-    availability_zone  = ["us-east-1a", "us-east-1b"]
-    internet_gw        = true
+      web = {
 
-  }
+        name              = "web"
+        cidr_block        = ["10.0.2.0/24", "10.0.3.0/24"]
+        nat_gw            = true
+        create_nat_gw     = true
 
-  web = {
+      }
 
-    name               = "web"
-    vpc_name           = "main"
-    cidr_block         = ["10.0.2.0/24", "10.0.3.0/24"]
-    availability_zone  = ["us-east-1a", "us-east-1b"]
-    nat_gw             = true
+      app = {
 
-  }
+        name              = "app"
+        cidr_block        = ["10.0.4.0/24", "10.0.5.0/24"]
+        nat_gw            = true
 
-  app = {
+      }
 
-    name               = "app"
-    vpc_name           = "main"
-    cidr_block         = ["10.0.4.0/24", "10.0.5.0/24"]
-    availability_zone  = ["us-east-1a", "us-east-1b"]
-    nat_gw             = true
+      db = {
 
-  }
+        name              = "db"
+        cidr_block        = ["10.0.6.0/24", "10.0.7.0/24"]
+        nat_gw            = true
 
-  db= {
-
-    name               = "db"
-    vpc_name           = "main"
-    cidr_block         = ["10.0.6.0/24", "10.0.7.0/24"]
-    availability_zone  = ["us-east-1a", "us-east-1b"]
-    nat_gw             = true
-
+      }
+    }
   }
 }
--- main.tf
+++ main.tf
@@ -4,27 +4,27 @@ module "vpc" {
   default_vpc_id         = var.default_vpc_id
   for_each               = var.vpc
   cidr_block             = each.value.cidr_block
-  public_subnet_ids      = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  subnets                = each.value.subnets
+  availability_zone      = each.value.availability_zone
     }
 
-module "subnets" {
-  source = "github.com/raja9542/tf-module-subnets.git"
-  env = var.env
-  default_vpc_id = var.default_vpc_id
-
-  for_each                  = var.subnets
-  cidr_block                = each.value.cidr_block
-  availability_zone         =each.value.availability_zone
-  name                      = each.value.name
-  # to get output of map of map variable and from another module use lookup function
-  vpc_id                    = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-  vpc_peering_connection_id  = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
-  internet_gw_id             = lookup(lookup(module.vpc, each.value.vpc_name, null), "internet_gw_id", null)
-  # look for each.value for internet_gw if it is there it will give true if not default value false
-  internet_gw                = lookup(each.value, "internet_gw", false )
-  nat_gw                     = lookup(each.value, "nat_gw", false )
-
-}
+#module "subnets" {
+#  source = "github.com/raja9542/tf-module-subnets.git"
+#  env = var.env
+#  default_vpc_id = var.default_vpc_id
+#
+#  for_each                  = var.subnets
+#  cidr_block                = each.value.cidr_block
+#  availability_zone         =each.value.availability_zone
+#  name                      = each.value.name
+#  # to get output of map of map variable and from another module use lookup function
+#  vpc_id                    = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+#  vpc_peering_connection_id  = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
+#  # look for each.value for internet_gw if it is there it will give true if not default value false
+#  internet_gw                = lookup(each.value, "internet_gw", false )
+#  nat_gw                     = lookup(each.value, "nat_gw", false )
+#
+#}
 
 output "subnet_ids" {
   value = module.subnets
